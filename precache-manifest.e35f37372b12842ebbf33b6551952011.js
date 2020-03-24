29f4ae951aa8e1991dc5234afe594aa163f2f4ca
jsina <jsina.maleki@gmail.com>
2020-03-25 02:38:50 +0430

    Updates

---
+++ precache-manifest.e35f37372b12842ebbf33b6551952011.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc430b43bfad75434cf42388700e653b",
+    "url": "/covid-2019/index.html"
+  },
+  {
+    "revision": "efc084614032417bdcfd",
+    "url": "/covid-2019/static/css/main.8b4427d9.chunk.css"
+  },
+  {
+    "revision": "56b974df4cc50aec2c77",
+    "url": "/covid-2019/static/js/2.17ee7d5d.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/covid-2019/static/js/2.17ee7d5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efc084614032417bdcfd",
+    "url": "/covid-2019/static/js/main.243cebdb.chunk.js"
+  },
+  {
+    "revision": "05825b01194397f1b73e",
+    "url": "/covid-2019/static/js/runtime-main.193135a4.js"
+  },
+  {
+    "revision": "50f4bba2f37081daeca8c423abdced8b",
+    "url": "/covid-2019/static/media/IRANSansWeb.50f4bba2.woff"
+  },
+  {
+    "revision": "fdc03aff6091bd153a3dc56b32ff1342",
+    "url": "/covid-2019/static/media/iran.fdc03aff.png"
+  }
+]);
