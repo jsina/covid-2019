76628c565ddb5b0be94d767e89c49240d059f0c5
jsina <jsina.maleki@gmail.com>
2020-03-24 18:19:42 +0430

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      class="flex justify-center bg-local h-screen bg-no-repeat bg-center bg-contain"
+      style={{
+        backgroundImage: `url(${require("./iran.png")})`
+      }}
+    >
+      <div className="w-full max-w-md"></div>
     </div>
   );
 }
